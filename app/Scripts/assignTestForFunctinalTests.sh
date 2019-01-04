f25872af6ba32cc667dd4995b1cead764f1998cd
maluyvova <vovaburyan@gmail.com>
2019-01-04 08:21:02 -0800

    sh

---
+++ app/Scripts/assignTestForFunctinalTests.sh
@@ -0,0 +1,8 @@
+#!/bin/sh -e
+TEST="$1"
+cd ..
+sed -i -e 's|testClassRegex="VladTest"|'"${TEST}"'|g' build.gradle
+echo "Running all tests "
+rm -R build.gradle-e
+
+#changing VladTest in build.gradle to given word to this script
