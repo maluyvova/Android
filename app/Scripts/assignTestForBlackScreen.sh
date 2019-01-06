9822f5d337a30b1f226566a2d7687cb03ecf24d5
maluyvova <vovaburyan@gmail.com>
2019-01-05 21:35:17 -0800

    script for BlackScreen tests

--- app/Scripts/assignTestForBlackScreen.sh
+++ app/Scripts/assignTestForBlackScreen.sh
@@ -2,7 +2,8 @@
 TEST="$1"
 cd ..
 sed -i -e 's|VladTest|'"${TEST}"'|g' build.gradle
-echo "Running ${TEST} test "
+sed -i -e 's|650000|650000000|g' build.gradle
+echo "Running ${TEST} test for 650000000 time "
 rm -R build.gradle-e
 
 #changing VladTest in build.gradle to given word to this script
